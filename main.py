68bf25d46672b49b135f3a15482580ca0300ca25
Damir <dadasay41@gmail.com>
2020-03-30 15:10:27 +0600

    Кнопки на клавиатуре PageUp, PageDown; Декоратор который ставить картинку карты;

--- main.py
+++ main.py
@@ -15,12 +15,19 @@ app = App(screen)
 running = True
 while running:
     for event in pygame.event.get():
+
         if (event.type == pygame.QUIT):
             terminate()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            # PgUp = 4 and PgDown = 5
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # MousePgUp = 4 and MousePgDown = 5
             if event.button == 4 or event.button == 5:
                 app.change_scale(event.button)
+        elif event.type == pygame.KEYDOWN:
+            # Keyboard PgUp = 4 and PgDown = 5
+            if event.key == pygame.K_PAGEDOWN:
+                app.change_scale(4)
+            elif event.key == pygame.K_PAGEUP:
+                app.change_scale(5)
     screen.fill((0, 0, 0))
     app.render()
     pygame.display.flip()
--- modules/App.py
+++ modules/App.py
@@ -5,6 +5,15 @@ import io
 from point_by_addr import get_pos
 
 
+def setPicture(func_setter):
+    def wrapper(self, *args, **kwargs):
+        func_setter(self, *args, **kwargs)
+        map_bytes = self.get_map_bytes(self.get_params_for_map())
+        self.map_picture = self.bytes_to_pygame_img(map_bytes)
+
+    return wrapper
+
+
 class App:
     def __init__(self, screen):
         self.screen = screen
@@ -12,33 +21,29 @@ class App:
         self.scale = CONST.START_SCALE  # [0, 17]
         self.position = CONST.START_POS
 
-        self.map_picture = None
-        self.map_bytes = self.get_map_bytes(self.get_params_for_map())
+        map_bytes = self.get_map_bytes(self.get_params_for_map())
+        self.map_picture = self.bytes_to_pygame_img(map_bytes)
 
-    def get_params_for_map(self):
+    def get_params_for_map(self) -> dict:
         return {'l': 'map',
                 "ll": ','.join(str(x) for x in self.position),
                 "z": self.scale}
 
-    def bytes_to_pygame_img(self, bytes):
+    def bytes_to_pygame_img(self, bytes) -> pygame.Surface:
         img = pygame.image.load(io.BytesIO(bytes))
         return pygame.transform.scale(img, CONST.MAP_SIZE)
-
-    def load_all(self) -> None:
-        self.map_picture = self.bytes_to_pygame_img(self.map_bytes)
 
     def get_map_bytes(self, params: dict) -> bytes:
         r = requests.get(CONST.API_STATIC_MAP, params=params)
         return r.content
 
     def render(self):
-        self.load_all()
         self.screen.blit(self.map_picture, CONST.MAP_POS)
 
+    @setPicture
     def change_scale(self, button_number):
         # PgUp = 4 and PgDown = 5
         if button_number == 4 and self.scale < 18:
             self.scale += 1
         if button_number == 5 and self.scale > 0:
             self.scale -= 1
-        self.map_bytes = self.get_map_bytes(self.get_params_for_map())
